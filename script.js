f7cf410e2c7f2963ec9c0eb8f3a608928a199992
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-02-20 16:31:18 -0300

    feat: create home section and menu effects

---
+++ script.js
@@ -0,0 +1,37 @@
+const menus = document.querySelectorAll('.header-menu a');
+
+const activeOnScroll = () => {
+  const [home, about, guide, products] = Array.from(menus);
+
+  const homeTop = document.querySelector('#main').scrollHeight;
+  const aboutTop = document.querySelector('#about').scrollHeight + homeTop;
+  const guideTop = document.querySelector('#guide').scrollHeight + aboutTop;
+
+  const scrollY = window.scrollY;
+
+  if (scrollY < homeTop) {
+    home.classList.add('menu-active');
+    about.classList.remove('menu-active');
+    guide.classList.remove('menu-active');
+    products.classList.remove('menu-active');
+  } else if (scrollY < aboutTop) {
+    about.classList.add('menu-active');
+    home.classList.remove('menu-active');
+    guide.classList.remove('menu-active');
+    products.classList.remove('menu-active');
+  } else if (scrollY < guideTop) {
+    guide.classList.add('menu-active');
+    home.classList.remove('menu-active');
+    about.classList.remove('menu-active');
+    products.classList.remove('menu-active');
+  } else {
+    products.classList.add('menu-active');
+    home.classList.remove('menu-active');
+    about.classList.remove('menu-active');
+    guide.classList.remove('menu-active');
+  }
+
+}
+
+activeOnScroll();
+window.addEventListener('scroll',activeOnScroll);
